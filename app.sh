0bf476cf2c5a3091d4c87f32a0800277795a1b83
Ricardo Padilha <ricardospadilha@gmail.com>
2015-06-30 10:19:10 -0700

    Updated to p7zip 9.38.1

--- app.sh
+++ app.sh
@@ -1,6 +1,6 @@
 ### P7ZIP ###
 _build_p7zip() {
-local VERSION="9.20.1"
+local VERSION="9.38.1"
 local FOLDER="p7zip_${VERSION}"
 local FILE="${FOLDER}_src_all.tar.bz2"
 local URL="http://sourceforge.net/projects/p7zip/files/p7zip/${VERSION}/${FILE}"
